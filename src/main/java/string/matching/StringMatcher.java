7229db03c307d1d4ffdeb21faaa34b3f6fac3804
Toptachamann <timofey.chudakov@gmail.com>
2018-03-28 18:27:02 +0300

    Naive matcher & Knuth-Morris-Pratt & Quick-Search & Tuned Boyer-Moore

--- src/main/java/string/matching/StringMatcher.java
+++ src/main/java/string/matching/StringMatcher.java
@@ -2,6 +2,10 @@ package string.matching;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class StringMatcher {
 
@@ -51,5 +55,79 @@ public class StringMatcher {
       prefixFunction.add(k);
     }
     return prefixFunction;
+  }
+
+  public static ArrayList<Integer> quickSearch(String text, String pattern) {
+    ArrayList<Integer> matched = new ArrayList<>();
+    Map<Character, Integer> badCharacter = badCharacter(getAlphabet(text, pattern), pattern);
+    int m = pattern.length();
+    for (int i = 0; i <= text.length() - pattern.length(); ) {
+      int j = m - 1;
+      for (; j >= 0; j--) {
+        if (pattern.charAt(j) != text.charAt(i + j)) {
+          break;
+        }
+      }
+      if (j == -1) {
+        matched.add(i);
+      }
+      i += badCharacter.get(text.charAt(i + m - 1));
+    }
+    return matched;
+  }
+
+  public static ArrayList<Integer> tunedBoyerMoore(String text, String pattern) {
+    ArrayList<Integer> matched = new ArrayList<>();
+    Map<Character, Integer> badCharacter = badCharacter(getAlphabet(text, pattern), pattern);
+    int m = pattern.length();
+    int n = text.length();
+    int shift = badCharacter.get(pattern.charAt(m - 1));
+    badCharacter.put(pattern.charAt(m - 1), 0);
+    int i = 0;
+    while (i < n) {
+      int k = badCharacter.get(text.charAt(i + m - 1));
+      while (k > 0) {
+        i += k;
+        k = badCharacter.get(text.charAt(i + m - 1));
+        i += k;
+        k = badCharacter.get(text.charAt(i + m - 1));
+        i += k;
+        k = badCharacter.get(text.charAt(i + m - 1));
+      }
+      int j = 0;
+      for (; j < m; j++) {
+        if (pattern.charAt(j) != text.charAt(i + j)) {
+          break;
+        }
+      }
+      if (j == m) {
+        matched.add(i);
+      }
+      i += shift;
+    }
+    return matched;
+  }
+
+  private static Set<Character> getAlphabet(String text, String pattern) {
+    Set<Character> alphabet = new HashSet<>();
+    for (int i = 0; i < text.length(); i++) {
+      alphabet.add(text.charAt(i));
+    }
+    for (int i = 0; i < pattern.length(); i++) {
+      alphabet.add(pattern.charAt(i));
+    }
+    return alphabet;
+  }
+
+  private static Map<Character, Integer> badCharacter(Set<Character> alphabet, String pattern) {
+    Map<Character, Integer> map = new HashMap<>();
+    int m = pattern.length();
+    for (Character c : alphabet) {
+      map.put(c, m);
+    }
+    for (int i = 0; i < m - 1; i++) {
+      map.put(pattern.charAt(i), m - i - 1);
+    }
+    return map;
   }
 }
--- src/test/groovy/string/matching/StringMatcherTest.groovy
+++ src/test/groovy/string/matching/StringMatcherTest.groovy
@@ -13,11 +13,34 @@ class StringMatcherTest extends Specification {
     where:
     text        | pattern | ans
     "abbaabbab" | "ab"    | [0, 4, 7]
+    "GAGAVAGAV" | "GAV"   | [2, 6]
   }
 
   @Unroll
   def "test KMP"() {
     def result = StringMatcher.KMP(text, pattern)
+    expect:
+    result == ans
+    where:
+    text        | pattern | ans
+    "abbaabbab" | "ab"    | [0, 4, 7]
+    "GAGAVAGAV" | "GAV"   | [2, 6]
+  }
+
+  @Unroll
+  def "text quick search"(){
+    def result = StringMatcher.quickSearch(text, pattern)
+    expect:
+    result == ans
+    where:
+    text        | pattern | ans
+    "abbaabbab" | "ab"    | [0, 4, 7]
+    "GAGAVAGAV" | "GAV"   | [2, 6]
+  }
+
+  @Unroll
+  def "text tuned Boyer-Moore"(){
+    def result = StringMatcher.tunedBoyerMoore(text, pattern)
     expect:
     result == ans
     where:
